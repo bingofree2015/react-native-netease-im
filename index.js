2e12998e345e86ca9944b5745fe13a48599f64c4
sky <sky@skys-iMac.local>
2018-02-03 20:52:36 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// import NIM from './NIM';
+import NimUtils from './Utils';
+
+
+import NimFriend from './im/Friend';
+import NimListener from './im/Listener';
+import NimSession from './im/Session';
+import NimSystemMsg from './im/SystemMsg';
+import NimTeam from './im/Team';
+export {
+    NimUtils,
+    NimFriend,
+    NimListener,
+    NimSession,
+    NimSystemMsg,
+    NimTeam,
+};
+
+// export default NIM;
